ba1785fb9d76bb7ef1e74571a9f244997356c4f3
y.mitani <y.mitani@cybermissions.co.jp>
2022-03-16 12:55:53 +0900

    質問一覧表示

--- components/stress-check.vue
+++ components/stress-check.vue
@@ -1,12 +1,37 @@
 <template>
   <v-app>
-    ストレスチェックです。
+    <div v-if="user">
+      ユーザー：{{ user.uid }}
+    </div>
+    <div v-else />
+    <div v-for="question in questions" :key="question.id">
+      {{ question.text }}
+    </div>
   </v-app>
 </template>
 
 <script>
+import { collection, onSnapshot } from '@firebase/firestore'
+import { onAuthStateChanged } from '@firebase/auth'
+import { auth, db } from '../plugins/firebase'
+const questionsCollectionRef = collection(db, 'questions')
+
 export default {
-  name: 'StressCheck'
+  name: 'StressCheck',
+  data () {
+    return {
+      questions: []
+    }
+  },
+  mounted () {
+    onAuthStateChanged(auth, (user) => {
+      this.user = user
+    })
+    onSnapshot(questionsCollectionRef, (querySnapshot) => {
+      this.questions = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }))
+      this.questions.sort((a, b) => b.timestamp - a.timestamp)
+    })
+  }
 }
 </script>
 
